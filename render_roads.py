1b1854d0e230c07d0a522d4d534b4c9a37868aae
lenik <lenik.terenin@drivemode.com>
2025-11-07 17:47:24 +0900

    tighten the layout

--- render_roads.py
+++ render_roads.py
@@ -15,14 +15,21 @@ place_name = "Kamppi, Helsinki, Finland"
 # This fetches the OSM street network as a NetworkX MultiDiGraph object.
 graph = ox.graph_from_place(place_name)
 
-# 3. Plot the street network
+# 3. Plot the street network with tight layout
 # This visualizes the downloaded street network using Matplotlib.
-fig, ax = ox.plot_graph(graph, show=False, close=False)
+fig, ax = ox.plot_graph(graph, show=False, close=False, 
+                        bbox_inches='tight', pad_inches=0.1)
 
 # Optional: Download and plot buildings
 area = ox.geocode_to_gdf(place_name)
 buildings = ox.features_from_place(place_name, tags={'building': True})
 buildings.plot(ax=ax, fc='gray', ec='none', alpha=0.7)
 
+# Remove margins and whitespace
+plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+ax.margins(0)
+ax.axis('off')  # Hide axes for cleaner look
+
 # 4. Display the plot
+plt.tight_layout(pad=0)
 plt.show()
